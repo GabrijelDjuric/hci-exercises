d22148f6b7c28eb7839af633043b92a78ed1010a
Vanja Đenadija <vanja.djenadija.pro@gmail.com>
2025-11-05 22:01:15 +0100

    feat: add book list with List<T> (BROKEN collection binding)
    - Use List<Book> for collection
    - Add ListBox to display books
    - Add button to insert new books
    - Problem: UI doesn't update when adding items
    - Demonstrates WHY we need ObservableCollection

---
+++ 03_DataBinding_ObservableCollection/BooksWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using _03_DataBinding_ObservableCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _03_DataBinding_ObservableCollection
+{
+    /// <summary>
+    /// Interaction logic for BooksWindow.xaml
+    /// </summary>
+    public partial class BooksWindow : Window
+    {
+
+        public List<Book> Books { get; set; } // ❌ List, not ObservableCollection
+
+        public BooksWindow()
+        {
+            InitializeComponent();
+
+            Books = new List<Book>
+            {
+                new Book { Title = "1984", Author = "George Orwell", Year = 1949, IsRead = false },
+                new Book { Title = "Brave New World", Author = "Aldous Huxley", Year = 1932, IsRead = true },
+                new Book { Title = "Fahrenheit 451", Author = "Ray Bradbury", Year = 1953, IsRead = false }
+            };
+
+            this.DataContext = this;
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+            var newBook = new Book
+            {
+                Title = "New Book",
+                Author = "Unknown",
+                Year = 2024,
+                IsRead = false
+            };
+
+            Books.Add(newBook);
+            // Problem: ListBox se NE ažurira! ❌
+        }
+    }
+}
